e8fbeabd9880fb59fd8361b00f3cc6ecad90f82f
Nestor Cofre <50756891+ncofre98@users.noreply.github.com>
2025-02-07 06:28:34 -0600

    Small improvement in alignment & JS targets only 'normal' buttons

--- js/events.js
+++ js/events.js
@@ -2,7 +2,7 @@ import { updateDisplay } from './ui.js';
 import { calculate } from './calculator.js';
 
 export function initEvents() {
-    document.querySelectorAll('.calculator__button').forEach(button => {
+    document.querySelectorAll('[class="calculator__button]').forEach(button => {
         button.addEventListener('click', () => {
             updateDisplay(calculate(button.textContent));
         })
